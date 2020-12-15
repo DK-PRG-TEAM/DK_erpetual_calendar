a40b2c788fd696e845a4570648fcdd9d9b243c2c
AnYing <anyingiit@aliyun.com>
2020-12-16 00:41:47 +0800

    成功打出2020年12月的日历

---
+++ consts.h
@@ -0,0 +1,6 @@
+#define ONE_YEAR_MONTH 12
+#define ONE_MONTH_DAY 30
+#define COMMON_YEAR_DAYS 365
+#define LEAP_YEAR_DAYS 366
+const int commonMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+const int leapYearMonth[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
---
+++ structs.h
@@ -0,0 +1,41 @@
+#define ONE_YEAR_MONTH 12
+//#define ONE_MONTH_DAY 30
+//#define COMMON_YEAR_DAYS 365
+//#define LEAP_YEAR_DAYS 366
+//const int commonMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+//const int leapYearMonth[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
+
+//typedef struct{
+//    const int ONE_YEAR_MONTH = 12;
+//    const int ONE_MONTH_DAY = 30;
+//    const int COMMON_YEAR_DAYS = 365;
+//    const int LEAP_YEAR_DAYS = 366;
+//    const int COMMON_MONTH[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
+//    const int LEAP_YEAR_MONTH[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
+//}StandardAboutDate;
+
+//StandardAboutDate standardAboutDate;
+//int test[1] = {0};
+
+typedef struct {
+    int monthNum;
+    struct {
+        int *p;
+        int length;
+    }data;
+}Month;
+
+
+typedef struct {
+    Month *month;
+    int length;
+}Months;
+
+typedef struct {
+    Months months;
+}Year;
+
+typedef struct {
+    Year *years;
+    int length;
+}Years;
