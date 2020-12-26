e6105e8d6694321e26997c6e3332b948e49c1b63
anyingiit <anyingiit@gmail.com>
2020-12-26 21:45:03 +0800

    增加使用蔡勒公示计算星期几的方法

--- src/Consts/include/consts.h
+++ src/Consts/include/consts.h
@@ -1,6 +1,10 @@
+#include <string>
+using namespace std;
+
 #define ONE_YEAR_MONTH 12
 #define ONE_MONTH_DAY 30
 #define COMMON_YEAR_DAYS 365
 #define LEAP_YEAR_DAYS 366
 const int commonMonth[12] = {31,28,31,30,31,30,31,31,30,31,30,31};
 const int leapYearMonth[12] = {31,29,31,30,31,30,31,31,30,31,30,31};
+const string week[7] = {"日", "一", "二", "三", "四", "五", "六"};
